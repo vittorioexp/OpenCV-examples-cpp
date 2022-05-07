2fd101c427bdbd43f3da43076d621d7f81b8e090
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2022-05-07 09:09:36 +0200

    initial commit

---
+++ src/4/lab4.cpp
@@ -0,0 +1,15 @@
+/*
+    Author: Vittorio Esposito
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Lab4 is running" << endl;
+
+    return 0;
+}
